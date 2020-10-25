66b7ee8fedd5d6e4e921e032ce837c61f17a582a
yuyuan <1455578041@qq.com>
2020-10-25 14:42:01 +0800

    初始化英雄池 优化效率

--- sysadmin/organization/src/main/java/com/springboot/cloud/sysadmin/organization/config/CombinationUtil.java
+++ sysadmin/organization/src/main/java/com/springboot/cloud/sysadmin/organization/config/CombinationUtil.java
@@ -37,6 +37,6 @@ public class CombinationUtil {
                 stack.pop();
             }
         }
-
     }
+
 }
--- sysadmin/organization/src/main/java/com/springboot/cloud/sysadmin/organization/config/HeroCombination.java
+++ sysadmin/organization/src/main/java/com/springboot/cloud/sysadmin/organization/config/HeroCombination.java
@@ -5,12 +5,22 @@ import java.util.*;
 public class HeroCombination {
     public static Stack<Hero> stack = new Stack<Hero>();
     public static Map<String, List<Integer>> effect = new HashMap<String, List<Integer>>();
+    public static Set<Hero> supSet = new HashSet<Hero>();//候选的英雄
     private static int count = 0;
 
     public static void main(String[] args) {
-        HeroCombination.effect = InitEffect.initEffect();
-        Hero[] heroes = InitHero.initHero();
-        combine(heroes, 8, 0, 0); // 从英雄池数组中选择N个英雄
+        String heroName = "腥红之月";
+        Integer heroAmount = 8;//英雄数量
+        Integer amount = 6;//羁绊数量
+        Integer combineAmount = 5;//羁绊组合数量
+        HeroCombination.effect = InitEffect.initEffect();//初始化羁绊特效
+//        Hero[] heroes = InitHero.initHero();//初始化英雄池
+        Map<String, Set<Hero>> heroes = InitHero.initHero(heroName);//初始化英雄池
+        combineCal(heroes.get("sup").toArray(new Hero[0]), heroes.get("main").size() - amount, 0, 0); // 选出候选英雄
+        Set<Hero> heroSet = heroes.get("main");
+        heroSet.addAll(supSet);
+        Hero[] heroeArray = heroSet.toArray(new Hero[0]);
+        combine(heroeArray, heroAmount, 0, 0); // 从英雄池数组中选择N个英雄
         System.out.println("组合次数：" + HeroCombination.count);
     }
 
@@ -55,7 +65,29 @@ public class HeroCombination {
                 stack.pop();
             }
         }
+    }
 
+    /**
+     * @param array        元素
+     * @param toPickNum    要选多少个元素
+     * @param selectedNum  当前有多少个元素
+     * @param currentIndex 当前选到的下标
+     */
+    private static void combineCal(Hero[] array, int toPickNum, int selectedNum, int currentIndex) {
+        if (selectedNum == toPickNum) {
+            stack.forEach(e -> {
+                supSet.add(e);
+            });
+            return;
+        }
+
+        for (int i = currentIndex; i < array.length; i++) {
+            if (!stack.contains(array[i])) {
+                stack.add(array[i]);
+                combineCal(array, toPickNum, selectedNum + 1, i);
+                stack.pop();
+            }
+        }
     }
 
 }
